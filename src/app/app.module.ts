3996a2a2ef8a80d111cd51c4fa9dfdf474e7b2db
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-25 10:54:57 +0100

    update bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { AddDrPipe } from './add-dr.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentListComponent
+    StudentListComponent,
+    AddDrPipe
   ],
   imports: [
     BrowserModule,
